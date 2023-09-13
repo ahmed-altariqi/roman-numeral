f2b7ff93e836d907647f2927a6b785ac4dd57576
Ahmed Altariqi <ahmed.altariqi1@gmail.com>
2023-09-13 17:53:07 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const ROMAN_NUMERALS = [
+  [1000, "M"],
+  [900, "CM"],
+  [500, "D"],
+  [400, "CD"],
+  [100, "C"],
+  [90, "XC"],
+  [50, "L"],
+  [40, "XL"],
+  [10, "X"],
+  [9, "IX"],
+  [5, "V"],
+  [4, "IV"],
+  [1, "I"],
+];
+
+function convertToRoman(num, result = "") {
+  for (const [value, romanCharacter] of ROMAN_NUMERALS) {
+    if (value <= num) {
+      result += romanCharacter;
+      num -= value;
+      if (num >= 1000 || num >= value) {
+        return convertToRoman(num, result);
+      }
+    }
+  }
+
+  return result;
+}
+
+const result = convertToRoman(2024); // { result: 'MMXXIV' }
